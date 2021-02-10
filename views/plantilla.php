abb835cf4a439a39a91e5d958be429829e3569c2
Frank <alexisgarciavillanueva@gmail.com>
2021-02-10 11:58:29 -0600

    Avance menu y header

--- views/plantilla.php
+++ views/plantilla.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cerrando el Ciclo A.C.</title>
 
-    <!-- ### NORMALIZE PARA REINICIAR MARGENES Y PADDINGS ### -->
-    <link rel="stylesheet" href="css/normalize.css">
-
-    <!-- ### MIS ESTILOS ### -->
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+    <!-- ### CDNs DE FUENTES ### -->
     
+    <!-- FONT-AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <!-- MONTSERRAT -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    
+    <!-- ### NORMALIZE PARA REINICIAR MARGENES Y PADDINGS ### -->
+    <link rel="stylesheet" href="views/css/normalize.css">
+    <!-- ### MIS ESTILOS ### -->
+    <link rel="stylesheet" href="views/css/style.css">
+</head>
+
+<body>
+
     <?php include_once "views/sections/header.php"; ?>
     <?php include_once "views/sections/nav.php"; ?>
     <?php include_once "views/pages/body.php"; ?>
     <?php include_once "views/sections/footer.php"; ?>
 
 </body>
+
 </html>
