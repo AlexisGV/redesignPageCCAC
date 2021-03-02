5fdaad3747f5d3b728fe81c2a6e6bc7fd3f140d7
Frank <alexisgarciavillanueva@gmail.com>
2021-03-02 12:40:27 -0600

    Avance Slide

--- views/pages/body.php
+++ views/pages/body.php
@@ -1,9 +1,49 @@
 <article>
     <div class="wrap">
 
-        <section class="slider__content">
-        
-            
+        <section class="slider">
+            <div class="slider__img__content" sliderId="1">
+                <img src="https://picsum.photos/id/10/1920/540" alt="">
+                <div class="slider__img__info">
+                    <h2 class="slider__img__title">Slider 01</h2>
+                    <div class="slider__img__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quod id, pariatur unde expedita molestias impedit. Tempore vero consequatur voluptas voluptatibus dolorem aut aliquam dignissimos.</div>
+                </div>
+            </div>
+            <div class="slider__img__content" sliderId="2">
+                <img src="https://picsum.photos/id/35/1920/540" alt="">
+                <div class="slider__img__info">
+                    <h2 class="slider__img__title">Slider 02</h2>
+                    <div class="slider__img__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quod id, pariatur unde expedita molestias impedit. Tempore vero consequatur voluptas voluptatibus dolorem aut aliquam dignissimos.</div>
+                </div>
+            </div>
+            <div class="slider__img__content" sliderId="3">
+                <img src="https://picsum.photos/id/114/1920/540" alt="">
+                <div class="slider__img__info">
+                    <h2 class="slider__img__title">Slider 03</h2>
+                    <div class="slider__img__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quod id, pariatur unde expedita molestias impedit. Tempore vero consequatur voluptas voluptatibus dolorem aut aliquam dignissimos.</div>
+                </div>
+            </div>
+            <div class="slider__img__content" sliderId="4">
+                <img src="https://picsum.photos/id/230/1920/540" alt="">
+                <div class="slider__img__info">
+                    <h2 class="slider__img__title">Slider 04</h2>
+                    <div class="slider__img__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quod id, pariatur unde expedita molestias impedit. Tempore vero consequatur voluptas voluptatibus dolorem aut aliquam dignissimos.</div>
+                </div>
+            </div>
+            <div class="slider__img__content" sliderId="5">
+                <img src="https://picsum.photos/id/545/1920/540" alt="">
+                <div class="slider__img__info">
+                    <h2 class="slider__img__title">Slider 05</h2>
+                    <div class="slider__img__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quod id, pariatur unde expedita molestias impedit. Tempore vero consequatur voluptas voluptatibus dolorem aut aliquam dignissimos.</div>
+                </div>
+            </div>
+            <div class="slider__navigation">
+                <div class="slider__navigation__btn"></div>
+                <div class="slider__navigation__btn"></div>
+                <div class="slider__navigation__btn"></div>
+                <div class="slider__navigation__btn"></div>
+                <div class="slider__navigation__btn"></div>
+            </div>
 
         </section>
 
